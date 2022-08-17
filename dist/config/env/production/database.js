53f8039fab57efbb99f53011543203ecd51a15c7
Michel Lamarlière <m.lamarliere@icloud.com>
2022-08-17 19:14:36 +0200

    feature: added a date alternative text

--- dist/config/env/production/database.js
+++ dist/config/env/production/database.js
@@ -1,21 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const parse = require("pg-connection-string").parse;
+const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
-
-exports.default = () => ({
-  connection: {
-    client: "postgres",
+exports.default = ({ env }) => ({
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+        client: 'postgres',
+        connection: {
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            user: config.user,
+            password: config.password,
+            ssl: {
+                rejectUnauthorized: false
+            },
+        },
+        debug: false,
     },
-    debug: false,
-  },
 });

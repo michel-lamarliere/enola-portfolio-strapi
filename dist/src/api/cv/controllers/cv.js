f3f2244ebba45ea63617adb409c3dda9a4bbce9f
Michel Lamarlière <m.lamarliere@icloud.com>
2022-09-15 14:57:23 +0200

    feature: trying to fix aws mistake

---
+++ dist/src/api/cv/controllers/cv.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ *  cv controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::cv.cv');
